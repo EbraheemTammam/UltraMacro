fb3c8d70f5550d23b473dc21b457e949ad87e1a9
Ebraheem <ebraheem20012020@gmail.com>
2024-02-29 14:27:38 +0200

    fixing import

--- mixins.py
+++ mixins.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Datetime
+from sqlalchemy import Column, DateTime
 from sqlalchemy.orm import declarative_mixin
 
 @declarative_mixin
 class Timestamp:
-	created_at = Column(Datetime, default=datetime.utcnow, nullable=False)
-	updated_at = Column(Datetime, default=datetime.utcnow, nullable=False)
+	created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+	updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
