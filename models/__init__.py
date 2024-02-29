a8a6aa73de15277e67133a9a35f1ab297b87a479
Ebraheem <ebraheem20012020@gmail.com>
2024-02-29 14:25:41 +0200

    user model

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from .regulation import Base
 from .department import Base
 from .division import Base
+from .user import Base
+
---
+++ models/user.py
@@ -0,0 +1,38 @@
+import uuid
+import enum
+from sqlalchemy import (
+	Column,
+	Integer,
+	String,
+	Boolean,
+	ForeignKey,
+	Enum,
+	Text,
+)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text, null
+from sqlalchemy_utils import EmailType, URLType, PasswordType
+
+from database.client import Base
+
+
+class User(Base):
+	__tablename__ = 'users'
+    
+	id = Column(
+		UUID(as_uuid=True),
+		primary_key=True,
+		index=True,
+		default=uuid.uuid4,
+		nullable=False
+	)
+	first_name = Column(String(50), nullable=False)
+	last_name = Column(String(50), nullable=False)
+	email = Column(EmailType, unique=True, nullable=False)
+	password = Column(
+		PasswordType(schemes=['bcrypt'], deprecated="auto"),
+		nullable=False
+	)
+	is_admin = Column(Boolean, nullable=False, default=False)
