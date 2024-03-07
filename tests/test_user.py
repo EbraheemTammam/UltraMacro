a18bd53e85174bee9f7b103dc441e9d719008677
Ebraheem <ebraheem20012020@gmail.com>
2024-03-07 14:18:31 +0200

    using authorized client

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,37 +2,38 @@ import pytest
 import uuid
 
 
-def test_get_all_users(client):
-    res = client.get(
+def test_get_all_users(authorized_client):
+    res = authorized_client.get(
         '/accounts',
     )
     assert res.status_code == 200
 
 
-def test_get_user(client, test_user):
+def test_get_user(authorized_client, test_user):
     user = test_user
-    res = client.get(
+    res = authorized_client.get(
         f'/accounts/{user["id"]}',
     )
     assert res.status_code == 200
 
 
-def test_get_non_existing_user(client):
-    res = client.get(
+def test_get_non_existing_user(authorized_client):
+    res = authorized_client.get(
         f'/accounts/{uuid.uuid4()}',
     )
     assert res.status_code == 404
 
 
-def test_update_user(client, test_user):
+def test_update_user(authorized_client, test_user):
     user = {
         **test_user,
         'first_name': "first",
         "last_name": "last",
         "email": "updated@example.com",
-        "is_admin": False
+        "is_admin": False,
+        'divisions': []
     }
-    res = client.put(
+    res = authorized_client.put(
         f'/accounts/{user["id"]}',
         json=user
     )
@@ -43,28 +44,32 @@ def test_update_user(client, test_user):
     assert res.json()["is_admin"] == False
 
 
-def test_update_non_existing_user(client):
+def test_update_non_existing_user(authorized_client):
     user = {
         'first_name': "first",
         "last_name": "last",
         "email": "updated@example.com",
         'password': 'password',
-        "is_admin": False
+        "is_admin": False,
+        'divisions': []
     }
-    res = client.put(
+    res = authorized_client.put(
         f'/accounts/{uuid.uuid4()}',
         json=user
     )
     assert res.status_code == 404
 
 
-def test_delete_user(client, test_user):
+def test_delete_user(authorized_client, test_user):
     user = test_user
-    res = client.delete(
+    res = authorized_client.delete(
         f'/accounts/{user["id"]}',
     )
     assert res.status_code == 204
-    res = client.delete(
+
+
+def test_delete_non_existing_user(authorized_client):
+    res = authorized_client.delete(
         f'/accounts/{uuid.uuid4()}',
     )
     assert res.status_code == 404
