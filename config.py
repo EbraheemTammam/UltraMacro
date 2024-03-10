d05f61a4082f2d670c841f56eec7efc4a9124e9b
Ebraheem <ebraheem20012020@gmail.com>
2024-03-10 16:50:01 +0200

    changing expire interval to hours

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
 	# jwt token settings
 	TOKEN_URL: str = 'login'
 	TOKEN_ENCODING_ALGORITHM: str = 'HS256'
-	ACCESS_TOKEN_EXPIRE_MINUTES: int = 720
+	ACCESS_TOKEN_EXPIRE_HOURS: int = 12
 	# database settings
 	DATABASE_URL: str = 'sqlite+sqlite3:///db.sqlite3'
 	ASYNC_DATABASE_URL: str = 'sqlite+aiosqlite:///db.sqlite3'
