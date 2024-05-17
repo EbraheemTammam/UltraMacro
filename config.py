db9416985f6c05cf72eb171bba40287853a76fdb
Ebraheem <ebraheem20012020@gmail.com>
2024-05-17 23:24:09 +0300

    custom logging

--- config.py
+++ config.py
@@ -3,7 +3,36 @@ from pydantic_settings import BaseSettings
 from importlib import import_module
 import logging
 
-logging.basicConfig(level=logging.INFO)
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;21m"
+    green = "\x1b[32m"
+    reset = "\x1b[0m"
+
+    FORMATS = {
+        logging.DEBUG: grey + "%(asctime)s - %(levelname)s" + reset +":\t %(message)s",
+        logging.INFO: green + "%(levelname)s" + reset + ":\t %(message)s",
+        logging.WARNING: grey + "%(asctime)s - %(levelname)s" + reset + ":\t %(message)s",
+        logging.ERROR: grey + "%(asctime)s - %(levelname)s" + reset + ":\t %(message)s",
+        logging.CRITICAL: grey + "%(asctime)s - %(levelname)s" + reset + ":\t %(message)s",
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+# Set up the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create a custom console handler and set the formatter
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(CustomFormatter())
+
+# Add the handler to the logger
+logger.addHandler(console_handler)
+
 
 class Settings(BaseSettings):
 
@@ -53,7 +82,7 @@ def get_base():
             module = import_module(f'{app}.models')
             logging.info(f'Successfully imported {app}.models')
         except Exception as e:
-            logging.warning(f'Failed to import {app}.models: {e}')
+            logging.warning(f'\tFailed to import {app}.models: {e}')
             continue
         base = getattr(module, 'Base', None)
         if base is not None:
