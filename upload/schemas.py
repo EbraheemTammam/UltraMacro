0d74a90a4d69475b7e0b063999ea1ce36d8f8928
Ebraheem <ebraheem20012020@gmail.com>
2024-06-30 22:42:40 +0300

    optional fields

--- upload/schemas.py
+++ upload/schemas.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class DivisionUploadResponse(BaseModel):
-    department_1_id: int
-    department_2_id: int
+    department_1_id: Optional[int] | None
+    department_2_id: Optional[int] | None
     name: str
     hours: int
     private: bool
